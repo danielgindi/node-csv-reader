fb9cbaf7cbc2ebe681933763ba257e9038809cff
Cameron Tacklind <cameron@tacklind.com>
2020-01-30 13:26:22 -0800

    Fix TypeScript API

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="node" />
 
-import { Transform } from "stream";
+import { Transform, TransformCallback, Readable } from "stream";
 
 declare type CsvReadableStreamOptions = {
   /**
@@ -73,12 +73,86 @@ declare type CsvReadableStreamOptions = {
 
 declare type Line = (string | number | boolean)[];
 
-export declare class CsvReadableStream extends Transform {
-  constructor(options?: CsvReadableStreamOptions);
+declare interface CsvReadableStream extends Transform {
+  new (options?: CsvReadableStreamOptions): this;
+  (options?: CsvReadableStreamOptions): this;
 
-  on(event: "data", cb: (line: Line) => void): CsvReadableStream;
+  addListener(event: "close", listener: () => void): this;
+  addListener(event: "data", listener: (line: Line) => void): this;
+  addListener(event: "end", listener: () => void): this;
+  addListener(event: "readable", listener: () => void): this;
+  addListener(event: "drain", listener: () => void): this;
+  addListener(event: "error", listener: (err: Error) => void): this;
+  addListener(event: "finish", listener: () => void): this;
+  addListener(event: "pipe", listener: (src: Readable) => void): this;
+  addListener(event: "unpipe", listener: (src: Readable) => void): this;
+  addListener(event: string | symbol, listener: (...args: any[]) => void): this;
+
+  on(event: "close", listener: () => void): this;
+  on(event: "data", listener: (line: Line) => void): this;
+  on(event: "end", listener: () => void): this;
+  on(event: "readable", listener: () => void): this;
+  on(event: "drain", listener: () => void): this;
+  on(event: "error", listener: (err: Error) => void): this;
+  on(event: "finish", listener: () => void): this;
+  on(event: "pipe", listener: (src: Readable) => void): this;
+  on(event: "unpipe", listener: (src: Readable) => void): this;
+  on(event: string | symbol, listener: (...args: any[]) => void): this;
+
+  once(event: "close", listener: () => void): this;
+  once(event: "data", listener: (line: Line) => void): this;
+  once(event: "end", listener: () => void): this;
+  once(event: "readable", listener: () => void): this;
+  once(event: "drain", listener: () => void): this;
+  once(event: "error", listener: (err: Error) => void): this;
+  once(event: "finish", listener: () => void): this;
+  once(event: "pipe", listener: (src: Readable) => void): this;
+  once(event: "unpipe", listener: (src: Readable) => void): this;
+  once(event: string | symbol, listener: (...args: any[]) => void): this;
+
+  prependListener(event: "close", listener: () => void): this;
+  prependListener(event: "data", listener: (line: Line) => void): this;
+  prependListener(event: "end", listener: () => void): this;
+  prependListener(event: "readable", listener: () => void): this;
+  prependListener(event: "drain", listener: () => void): this;
+  prependListener(event: "error", listener: (err: Error) => void): this;
+  prependListener(event: "finish", listener: () => void): this;
+  prependListener(event: "pipe", listener: (src: Readable) => void): this;
+  prependListener(event: "unpipe", listener: (src: Readable) => void): this;
+  prependListener(
+    event: string | symbol,
+    listener: (...args: any[]) => void
+  ): this;
+
+  prependOnceListener(event: "close", listener: () => void): this;
+  prependOnceListener(event: "data", listener: (line: Line) => void): this;
+  prependOnceListener(event: "end", listener: () => void): this;
+  prependOnceListener(event: "readable", listener: () => void): this;
+  prependOnceListener(event: "drain", listener: () => void): this;
+  prependOnceListener(event: "error", listener: (err: Error) => void): this;
+  prependOnceListener(event: "finish", listener: () => void): this;
+  prependOnceListener(event: "pipe", listener: (src: Readable) => void): this;
+  prependOnceListener(event: "unpipe", listener: (src: Readable) => void): this;
+  prependOnceListener(
+    event: string | symbol,
+    listener: (...args: any[]) => void
+  ): this;
+
+  removeListener(event: "close", listener: () => void): this;
+  removeListener(event: "data", listener: (line: Line) => void): this;
+  removeListener(event: "end", listener: () => void): this;
+  removeListener(event: "readable", listener: () => void): this;
+  removeListener(event: "drain", listener: () => void): this;
+  removeListener(event: "error", listener: (err: Error) => void): this;
+  removeListener(event: "finish", listener: () => void): this;
+  removeListener(event: "pipe", listener: (src: Readable) => void): this;
+  removeListener(event: "unpipe", listener: (src: Readable) => void): this;
+  removeListener(
+    event: string | symbol,
+    listener: (...args: any[]) => void
+  ): this;
 }
 
-export declare function CsvReadableStream(
-  options?: CsvReadableStreamOptions
-): CsvReadableStream;
+declare const CsvReadableStream: CsvReadableStream;
+
+export = CsvReadableStream;
